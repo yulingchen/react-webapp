1b83adbda6896c51b410f5f8c1d95f1028ffb112
luckcoding <luckcoding@gmail.com>
2016-04-27 15:57:07 +0800

    svg update

---
+++ src/components/SVG/ArrowRight.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SvgIcon from 'material-ui/lib/svg-icon'
+
+const ArrowRight = (props) => (
+  <SvgIcon {...props}>
+    <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
+    <path d="M0-.25h24v24H0z" fill="none"/>
+  </SvgIcon>
+)
+
+export default ArrowRight
